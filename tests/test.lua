d75d71096045d84aee8dac2dbe94e0291fd21d9a
sty00A4 <99866356+sty00A4@users.noreply.github.com>
2022-11-18 09:44:05 +0100

    unused keywords

--- tests/test.lua
+++ tests/test.lua
@@ -1,6 +1,1 @@
-function prime (x)
-if x <= 1 then return false end
-for n = 2, x - 1 do if (x % n) == 0 then return false end end
-return true
-end
-for x = 0, 1000 do if prime(x) then io.write(x .. "\9") end end
+print(not true)
