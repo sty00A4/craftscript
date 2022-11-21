430260509fb744637b5c5e97a8d946713d32ad7a
sty00A4 <99866356+sty00A4@users.noreply.github.com>
2022-11-21 20:23:38 +0100

    each file

--- installs/install_cc.lua
+++ installs/install_cc.lua
@@ -1,4 +1,12 @@
-shell.run("wget https://github.com/sty00A4/craftscript.git crs")
+fs.makeDir("crs")
+shell.run("wget https://raw.githubusercontent.com/sty00A4/craftscript/main/crs.lua crs/crs.lua")
+shell.run("wget https://raw.githubusercontent.com/sty00A4/craftscript/main/LICENSE crs/LICENSE")
+fs.makeDir("crs/src")
+shell.run("wget https://raw.githubusercontent.com/sty00A4/craftscript/main/src/global.lua crs/src/global.lua")
+shell.run("wget https://raw.githubusercontent.com/sty00A4/craftscript/main/src/init.lua crs/src/init.lua")
+shell.run("wget https://raw.githubusercontent.com/sty00A4/craftscript/main/src/lexer.lua crs/src/lexer.lua")
+shell.run("wget https://raw.githubusercontent.com/sty00A4/craftscript/main/src/parser.lua crs/src/parser.lua")
+shell.run("wget https://raw.githubusercontent.com/sty00A4/craftscript/main/src/position.lua crs/src/position.lua")
 fs.delete("crs/.gitignore")
 fs.delete("crs/installs")
 fs.delete("crs/tests")
@@ -6,7 +14,7 @@ local completion = require "cc.shell.completion"
 shell.setPath(shell.path()..":/crs")
 local crsComplete = completion.build(
     { completion.choice, { "run", "comp" } },
-    { completion.file, many = true },
-    { completion.file, many = true }
+    completion.file,
+    completion.file
 )
 shell.setCompletionFunction("crs/crs.lua", crsComplete)
